8d7c0b961c3fa2ee05db2e4e29e47a636d527417
bharathkumar.konderi <bharathk191@gmail.com>
2024-04-23 18:00:38 +0530

    myfirstproject

---
+++ test.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+import keyboard
+import sys
+
+pyautogui.FAILSAFE = False
+
+while True:
+    time.sleep(5)
+    for i in range(0, 100):
+        pyautogui.moveTo(0, i * 5)
+        for i in range(0, 3):
+            pyautogui.press('shift')
+        if keyboard.is_pressed('Q'):
+            sys.exit()
